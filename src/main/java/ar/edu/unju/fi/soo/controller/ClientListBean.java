300d4d6fecf82d672bdbb62f48d1998e5d2a1123
Ezequiel Montes <emontes@theinit.com>
2016-11-23 02:28:11 -0300

    Add views and controllers for client and vehicles.

---
+++ src/main/java/ar/edu/unju/fi/soo/controller/ClientListBean.java
@@ -0,0 +1,50 @@
+package ar.edu.unju.fi.soo.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import ar.edu.unju.fi.soo.model.Client;
+import ar.edu.unju.fi.soo.services.AgencyService;
+
+@ManagedBean(name = "clientList")
+public class ClientListBean implements Serializable {
+
+	private static final long serialVersionUID = 7784570305701107835L;
+
+	@ManagedProperty("#{agencyServiceImpl}")
+	private AgencyService agencyService;
+
+	private List<Client> list;
+
+	@PostConstruct
+	public void init() {
+		list = agencyService.listClients();
+	}
+
+	public void remove(Client client) {
+		// call to delete method in the service
+		// and reload the list.
+		list = agencyService.listClients();
+	}
+
+	public AgencyService getAgencyService() {
+		return agencyService;
+	}
+
+	public void setAgencyService(AgencyService agencyService) {
+		this.agencyService = agencyService;
+	}
+
+	public List<Client> getList() {
+		return list;
+	}
+
+	public void setList(List<Client> list) {
+		this.list = list;
+	}
+
+}
---
+++ src/main/java/ar/edu/unju/fi/soo/controller/VehicleListBean.java
@@ -0,0 +1,44 @@
+package ar.edu.unju.fi.soo.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import ar.edu.unju.fi.soo.model.Vehicle;
+import ar.edu.unju.fi.soo.services.AgencyService;
+
+@ManagedBean(name = "vehicleList")
+public class VehicleListBean implements Serializable {
+
+	private static final long serialVersionUID = -6392690264935189017L;
+
+	@ManagedProperty("#{agencyServiceImpl}")
+	private AgencyService agencyService;
+
+	private List<Vehicle> list;
+
+	@PostConstruct
+	public void init() {
+		list = agencyService.listVehicles();
+	}
+
+	public AgencyService getAgencyService() {
+		return agencyService;
+	}
+
+	public void setAgencyService(AgencyService agencyService) {
+		this.agencyService = agencyService;
+	}
+
+	public List<Vehicle> getList() {
+		return list;
+	}
+
+	public void setList(List<Vehicle> list) {
+		this.list = list;
+	}
+
+}
